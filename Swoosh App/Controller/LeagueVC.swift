8ce673fc09df84ff18d605a61c81f613049f0d63
Ville-Matti Hakanpää <villematti@villematti.info>
2021-04-18 20:17:44 +0300

    Add Player struct

--- Swoosh App/Controller/LeagueVC.swift
+++ Swoosh App/Controller/LeagueVC.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class LeagueVC: UIViewController {
+    
+    var player: Player!
 
+    @IBOutlet weak var nextBtn: BorderButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        player = Player()
     }
     
 
@@ -21,4 +24,20 @@ class LeagueVC: UIViewController {
         performSegue(withIdentifier: "skillVCSeque", sender: self)
     }
 
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
 }
---
+++ Swoosh App/Model/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  Swoosh App
+//
+//  Created by Ville-Matti Hakanpää on 18.4.2021.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
