e99bc685d073270ed2bcf5e6f73a218ada077a10
Ville-Matti Hakanpää <villematti@villematti.info>
2021-04-18 15:42:11 +0300

    Initial Commit

---
+++ Swoosh App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swoosh App
+//
+//  Created by Ville-Matti Hakanpää on 18.4.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Swoosh AppTests/Swoosh_AppTests.swift
@@ -0,0 +1,33 @@
+//
+//  Swoosh_AppTests.swift
+//  Swoosh AppTests
+//
+//  Created by Ville-Matti Hakanpää on 18.4.2021.
+//
+
+import XCTest
+@testable import Swoosh_App
+
+class Swoosh_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
