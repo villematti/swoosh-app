6ba6ea45d2b7bea1473ab97c637ba6dec6387c39
Ville-Matti Hakanpää <villematti@villematti.info>
2021-04-18 19:38:00 +0300

    Add ViewControllers

---
+++ Swoosh App/Controller/LeagueVC.swift
@@ -0,0 +1,29 @@
+//
+//  LeagueVC.swift
+//  Swoosh App
+//
+//  Created by Ville-Matti Hakanpää on 18.4.2021.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
