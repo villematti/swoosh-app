96313d823e23eeab84e00c46030ec17845295b7a
Ville-Matti Hakanpää <villematti@villematti.info>
2021-04-18 19:53:52 +0300

    Add seques programatically

--- Swoosh App/Controller/LeagueVC.swift
+++ Swoosh App/Controller/LeagueVC.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +17,8 @@ class LeagueVC: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSeque", sender: self)
     }
-    */
 
 }
